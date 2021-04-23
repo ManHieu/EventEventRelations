bb71984fb7ce771481612ee33145681f9610ef31
ManHieu <hieuman2708@gmail.com>
2021-04-23 11:24:35 +0700

    add: I2B2 weights

--- models/roberta_model.py
+++ models/roberta_model.py
@@ -24,6 +24,8 @@ class ECIRoberta(nn.Module):
             weights = [993.0/333, 993.0/349, 933.0/128, 933.0/453]
         if dataset == "MATRES":
             weights = [6404.0/3233, 6404.0/2263, 6404.0/232, 6404.0/676,]
+        if dataset == "I2B2":
+            weights = [1, 1, 1,]
         weights = torch.tensor(weights)
         if loss == None:
             self.loss = nn.CrossEntropyLoss(weight=weights)
