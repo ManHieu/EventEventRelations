99346efc4f463cde0e5dd3f2beca9666dae15bdf
Man Hieu <hieuman2708@gmail.com>
2021-05-08 09:50:45 +0700

    change: param space

--- ECI_main.py
+++ ECI_main.py
@@ -15,8 +15,8 @@ def count_parameters(model):
 
 def objective(trial:optuna.Trial):
     params = {
-        "bert_learning_rate": trial.suggest_categorical("b_lr", [1e-8, 5e-8, 1e-7, 5e-7, 1e-6, 5e-6, 1e-5]),
-        "mlp_learning_rate": trial.suggest_categorical('mlp_lr', [1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3]),
+        "bert_learning_rate": trial.suggest_categorical("b_lr", [1e-8, 5e-8, 1e-7, 5e-7, 1e-6, 5e-6]),
+        "mlp_learning_rate": trial.suggest_categorical('mlp_lr', [1e-7, 5e-7, 1e-6, 5e-6, 1e-5, 5e-5]),
         "MLP size": trial.suggest_categorical("MLP size", [256, 512, 768]),
         "early_stop": 100,
         'weight_decay': trial.suggest_float("weight_decay", 0, 1, step=0.2),
@@ -44,7 +44,7 @@ def objective(trial:optuna.Trial):
 
     with open("{}{}.txt".format(result_folder, dataset), 'a', encoding='UTF-8') as f:
         f.write("\n -------------------------------------------- \n")
-        f.write(" F1: {} \n CM: {} \n Hypeparameter: {} ".format(f1, CM, params))
+        f.write(" F1: \n {} \n CM: \n{} \n Hypeparameter: \n {} ".format(f1, CM, params))
     return f1
 
 if __name__=="__main__":
