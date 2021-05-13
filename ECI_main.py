2af563ec811a73d07fc9e2f13ff476fd1c5cdbd7
Man Hieu <hieuman2708@gmail.com>
2021-05-13 22:36:13 +0700

    change params space

--- ECI_main.py
+++ ECI_main.py
@@ -16,11 +16,11 @@ def count_parameters(model):
 
 def objective(trial:optuna.Trial):
     params = {
-        "bert_learning_rate": trial.suggest_categorical("b_lr", [1e-8, 5e-8, 1e-7, 5e-7, 1e-6, 5e-6, 1e-5, 5e-5, 1e-4,]),
-        "mlp_learning_rate":trial.suggest_categorical("m_lr", [1e-6, 1e-5, 1e-4]),
+        "bert_learning_rate": trial.suggest_categorical("b_lr", [5e-8, 8e-8, 1e-7, 3e-7, 5e-7, 8e-7, 1e-6]),
+        "mlp_learning_rate":trial.suggest_categorical("m_lr", [1e-5, 3e-5, 5e-5, 8e-5, 1e-4]),
         "MLP size": trial.suggest_categorical("MLP size", [512, 768]),
         "epoches": trial.suggest_categorical("epoches", [3, 6, 5, 9]),
-        'weight_decay': trial.suggest_float("weight_decay", 0, 0.6, step=0.2),
+        'weight_decay': trial.suggest_float("weight_decay", 0, 0.4, step=0.2),
         'negative_slope': trial.suggest_float("negative_slope", 0, 0.5, step=0.1),
         'warmup_proportion': 0.1,
     }
