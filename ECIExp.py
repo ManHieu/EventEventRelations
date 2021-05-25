02f7e0b7ca527ab6935dc36750e19af92b2947c3
Man Hieu <hieuman2708@gmail.com>
2021-05-25 10:27:53 +0700

    init

--- ECIExp.py
+++ ECIExp.py
@@ -92,7 +92,7 @@ class EXP():
             b_lambda = linear_lr_lambda
         else:
             print("We haven't support that lambda!!")
-        lamd = [b_lambda] * 8
+        lamd = [b_lambda] * 10
         mlp_lambda = [m_lr_lambda] * 2
         lamd.extend(mlp_lambda)
         
