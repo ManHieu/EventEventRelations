da130613ff25a9efaf52389385c785b1ec4a6d18
Man Hieu <hieuman2708@gmail.com>
2021-05-28 22:29:35 +0700

    add: sub attn sentence

--- models/roberta_model_multi.py
+++ models/roberta_model_multi.py
@@ -213,7 +213,7 @@ class ECIRobertaJointTask(nn.Module):
             mul_s = torch.mul(x, y)
             presentation = torch.cat([output_A, output_B, mul, sub_s], 1)
         if self.sub==False and self.sub==False:
-            presentation = torch.cat([output_A, output_B], 1)
+            presentation = torch.cat([output_A, output_B, sub_s], 1)
         
         # print(presentation.size())
         loss = 0.0
