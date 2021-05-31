fe4634c2e29ec11c57faa87bdd6f3a4aa880eb0a
Man Hieu <hieuman2708@gmail.com>
2021-05-31 19:14:22 +0700

    change: ib2b + matres only

--- ECI_main.py
+++ ECI_main.py
@@ -29,7 +29,7 @@ def objective(trial:optuna.Trial):
         'b_lr_decay_rate': 0.7,
         # trial.suggest_float('decay_rate', 0.7, 0.8, step=0.1),
         "task_weights": {
-            '1': trial.suggest_float('HiEve_weight', 0.4, 1, step=0.2), # 1 is HiEve
+            # '1': trial.suggest_float('HiEve_weight', 0.4, 1, step=0.2), # 1 is HiEve
             '2': 1, # 2 is MATRES.
             '3': trial.suggest_float('I2B2_weight', 0.4, 1, step=0.2),
         },
